a306726727fee00de8f1fe00c38b00dc85a84796
Arthur Adriel <arthurtorquato25@gmail.com>
2026-03-19 12:47:33 -0300

    Node.js
    Aprendendo GET e POST

---
+++ controllers/atendimentos.js
@@ -0,0 +1,5 @@
+module.exports = app => {
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está dando um GET e o galo é gigante!'))
+
+    app.post('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está dando um POST e o galo é copeiro!'))
+}
