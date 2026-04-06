37705031d214ce9591f0ed46aae8a87a4094f534
Arthur Adriel <arthurtorquato25@gmail.com>
2026-04-06 11:08:37 -0300

    feat

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,7 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está dando um GET e o galo é gigante!'))
 
-    app.post('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está dando um POST e o galo é copeiro!'))
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Você está na rota de atendimentos e está dando um POST e o galo é copeiro!')})
 }
