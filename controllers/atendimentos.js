b2561d9a6f41dd60634654235c8d1106742b7dd8
Arthur Adriel <arthurtorquato25@gmail.com>
2026-05-07 12:53:47 -0300

    .

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,7 +1,15 @@
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está dando um GET e o galo é gigante!'))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.buscarPorId(id, res)
+    })
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
